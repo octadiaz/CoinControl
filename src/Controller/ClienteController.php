7c944cb5e099bdfb821e797be520d3ed7c939bb8
Octavio Diaz <octaviodiaz262@gmail.com>
2024-03-05 16:06:35 -0300

    Fixes transaction screen and data analysis.

--- src/Controller/ClienteController.php
+++ src/Controller/ClienteController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+
 class ClienteController extends AbstractController
 {
     #[Route('/cliente', name: 'app_cliente')]
--- src/Controller/RegistroController.php
+++ src/Controller/RegistroController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Cliente;
 
 class RegistroController extends AbstractController
 {
@@ -14,5 +15,25 @@ class RegistroController extends AbstractController
         return $this->render('registro/registro.html.twig', [
             'controller_name' => 'RegistroController',
         ]);
+    }
+
+    public function Registro(){
+        $entityManager = $this->getDoctrine()->getManager();
+        
+        $Cliente = new Cliente();
+
+        //datos
+        $Cliente -> setUsername('');
+        $Cliente -> setPassword('');
+        $Cliente -> setNombre('');
+        $Cliente -> setApellido('');
+        $Cliente ->setEmail('');
+        $Cliente -> setDni('');
+
+        //doctrine
+        $entityManager->persist($Cliente);
+
+        //base de datos
+        $entityManager->flush();
     }
 }
