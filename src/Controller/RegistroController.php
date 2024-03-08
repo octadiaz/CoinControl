3258a86fba0388de32ff43867aa60bdcab8e53de
Gabriel Rodriguez <gabriel.ar.99@hotmail.com>
2024-03-08 00:54:59 -0300

    Verification added to register

--- src/Controller/RegistroController.php
+++ src/Controller/RegistroController.php
@@ -19,41 +19,60 @@ class RegistroController extends AbstractController
         $this->entityManager = $entityManager;
     }
 
-    
     #[Route("/registro", name :"registro_u", methods: ["GET", "POST"])]
     public function registroUsuario(Request $request): Response
     {
         // Si el formulario ha sido enviado
         if ($request->isMethod('POST'))
         {
-            // Obtener los datos del formulario
+            // Obtiene los datos del formulario
             $nombre = $request->request->get('nombre');
             $apellido = $request->request->get('apellido');
             $mail = $request->request->get('mail');
             $dni = $request->request->get('dni');
             $user = $request->request->get('user');
             $password = $request->request->get('pass');
-            $cliente = new Cliente();
-            $cliente->setNombre($nombre);
-            $cliente->setApellido($apellido);
-            $cliente->setEmail($mail);
-            $cliente->setDni($dni);
-            $cliente->setUsername($user);
-            $cliente->setPassword($password);
 
-            // Persistir los datos en la base de datos
-            $this->entityManager->persist($cliente);
-            $this->entityManager->flush();
+            // Verifica si todos los campos están completos
+            if ($nombre && $apellido && $mail && $dni && $user && $password)
+            {
+                // Verifica si el nombre de usuario ya existe en la base de datos
+                $existingCliente = $this->entityManager->getRepository(Cliente::class)->findOneBy(['username' => $user]);
+                
+                if ($existingCliente) 
+                {
+                    // Si el usuario ya existe, muestra un mensaje de error
+                    $this->addFlash('existent', 'Usuario existente');
+                    return $this->redirectToRoute('registro_u');
+                }
 
-            $this->addFlash('exito', 'Se ha registrado exitosamente');
+                // Crear una nueva instancia de Cliente y establecer sus propiedades
+                $cliente = new Cliente();
+                $cliente->setNombre($nombre);
+                $cliente->setApellido($apellido);
+                $cliente->setEmail($mail);
+                $cliente->setDni($dni);
+                $cliente->setUsername($user);
+                $cliente->setPassword($password);
 
-            // Redirigir a alguna página de confirmación o a la página de inicio
-            return $this->render('registro/registro.html.twig');
+                // Persistir los datos en la base de datos
+                $this->entityManager->persist($cliente);
+                $this->entityManager->flush();
 
+                $this->addFlash('exito', 'Se ha registrado exitosamente');
+
+                // Redirigir a alguna página de confirmación o a la página de inicio
+                return $this->redirectToRoute('registro_u');
+            } else {
+                // Si alguno de los campos está vacío, muestra un mensaje de error
+                $this->addFlash('complete', 'Complete todos los campos');
+                return $this->redirectToRoute('registro_u');
+            }
         }
-        // Renderizar la página Twig para cargar perfiles
+        // Si el método no es POST, simplemente renderiza el formulario de ingreso
         return $this->render('registro/registro.html.twig');
     }
+
 
     #[Route('/ingreso', name:'ingreso_u', methods: ['GET', "POST"])]
     public function ingresoUsuario(Request $request): Response 
